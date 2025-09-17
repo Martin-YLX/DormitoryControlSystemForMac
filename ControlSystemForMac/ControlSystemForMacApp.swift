7ead5ef4808daed94d82b9bda994d399f1392f4d
Martin-YLX <martin_ylx@outlook.com>
2025-09-17 11:07:08 +0800

    Initial Commit

---
+++ ControlSystemForMac/ControlSystemForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlSystemForMacApp.swift
+//  ControlSystemForMac
+//
+//  Created by Martin on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ControlSystemForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ControlSystemForMacTests/ControlSystemForMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  ControlSystemForMacTests.swift
+//  ControlSystemForMacTests
+//
+//  Created by Martin on 9/17/25.
+//
+
+import Testing
+@testable import ControlSystemForMac
+
+struct ControlSystemForMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
